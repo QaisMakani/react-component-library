d8b045c581caa1b89c86447fc5971f3ae8a24d96
Qais Makani <qais.makani@gmail.com>
2021-01-09 23:00:14 +0530

    Cleanup - Untracking Build Folder, and configuring ignore files

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.ts",
+  entry: './src/index.ts',
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "[name].js",
-    library: "react-component-library",
-    libraryTarget: "umd"
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js',
+    library: 'react-component-library',
+    libraryTarget: 'umd',
   },
   resolve: {
-    extensions: [".ts", ".tsx"]
+    extensions: ['.ts', '.tsx'],
   },
   module: {
     rules: [
@@ -17,19 +17,19 @@ module.exports = {
         test: /\.(ts|tsx)$/,
         use: [
           {
-            loader: require.resolve("ts-loader")
-          }
-        ]
+            loader: require.resolve('ts-loader'),
+          },
+        ],
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-        include: path.resolve(__dirname, "./src")
-      }
-    ]
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        include: path.resolve(__dirname, './src'),
+      },
+    ],
   },
   externals: {
-    react: "react",
-    "react-dom": "react-dom"
-  }
+    react: 'react',
+    'react-dom': 'react-dom',
+  },
 };
